41dbd8267c4612e5d0149ff1041fbe41087ea48c
Victor <victor.marco2002@gmail.com>
2025-11-03 21:03:09 -0300

    v0.4 (modal! não dá tempo de fazer nada nesse cão)

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+document.getElementById("musicForm").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-document.getElementById("musicForm").addEventListener("submit", (e) => {e.preventDefault();
+  const race = document.getElementById("race").value;
+  const classe = document.getElementById("classe").value;
+  const alignment = document.getElementById("alignment").value;
+  const modal = document.getElementById("resultModal");
+  const closeModal = document.getElementById("closeModal");
+  const result = document.getElementById("result");
 
-    const race = document.getElementById("race").value;
-    const classe = document.getElementById("classe").value;
-    const alignment = document.getElementById("alignment").value;
-    
-    const result = document.getElementById("result");
+  // Monta o resultado dentro do modal
+  modal.querySelector("h2").innerHTML = `
+    Você escolheu:<br><br>
+    Raça: <br>${race}<br>
+    Classe: ${classe}<br>
+    Alinhamento: ${alignment}
+  `;
 
-    result.innerHTML = `
+  // Exibe o modal
+  modal.style.display = 'flex';
 
-
-    <h2>Você escolheu: </h2>
-    <ul>
-        <li>Raça: ${race}</li>
-        <li>Classe: ${classe}</li>
-        <li>Alinhamento: ${alignment}</li>
-    </ul>
-    
-
-
-
-
-`;
+  // Fecha o modal ao clicar no botão "Voltar"
+  closeModal.addEventListener("click", () => {
+    modal.style.display = 'none';
+  });
 });
